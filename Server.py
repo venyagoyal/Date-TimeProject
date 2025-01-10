97728ad95aacb2e2674e0b56913da74b84a95442
venyagoyal <vgoyal@skywarditsolutions.com>
2025-01-10 10:15:38 -0500

    Update Server.py
    Added date and time resources

--- Server.py
+++ Server.py
@@ -1,8 +1,17 @@
-# here we will create a server 
 from mcp.server.fastmcp import FastMCP
+from datetime import datetime
 
-# Create an MCP server
 mcp = FastMCP("Demo")
 
+@mcp.resource("date://current")
+def get_current_date() -> str:
+    """Return the current date as a string."""
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    return f"The current date is {current_date}"
 
+@mcp.resource("time://current")
+def get_current_time() -> str:
+    """Return the current time as a string."""
+    current_time = datetime.now().strftime("%H:%M:%S")
+    print(f"The current time is: {current_time}")
 
